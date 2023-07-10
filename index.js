a4e1da842b97588b0ea4768f39226de6536f5068
NikolaKasabov <n_kasabov@mail.bg>
2023-07-11 01:04:22 +0300

    phonebook backend step 2 completed

--- index.js
+++ index.js
@@ -24,11 +24,22 @@ const persons = [
   }
 ];
 
+
 app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  const numberOfPeople = persons.length;
+  const date = new Date();
+
+  response.send(`
+    <p>Phonebook has info for ${numberOfPeople} people.</p>
+    <p>${date}</p>
+  `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 });
