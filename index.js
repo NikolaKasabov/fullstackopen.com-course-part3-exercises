58df2bebcdf766e1735920828c9bcf84810d3aa5
NikolaKasabov <n_kasabov@mail.bg>
2023-07-11 02:15:59 +0300

    phonebook backend step 6 completed

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ let persons = [
   }
 ];
 
+// check if the name is already added, returns boolean
+function doesNameExist(name) {
+  return !!persons.find(person => person.name.toLowerCase() === name.toLowerCase());
+}
+
 // get all persons
 app.get('/api/persons', (request, response) => {
   response.json(persons);
@@ -64,6 +69,25 @@ app.delete('/api/persons/:id', (request, response) => {
 // add a new person
 app.post('/api/persons', (req, res) => {
   const personData = req.body;
+
+  // check if the name is missing in the request
+  if (!personData.name) {
+    res.status(422).json({ error: 'the name is missing' });
+    return;
+  }
+
+  // check if the number is missing in the request
+  if (!personData.number) {
+    res.status(422).json({ error: 'the number is missing' });
+    return;
+  }
+
+  // check if the name is already in the list of persons
+  if (doesNameExist(personData.name)) {
+    res.status(422).json({ error: 'name must be unique' });
+    return;
+  }
+
   const newId = Math.floor(Math.random() * 1000000);
   const newPerson = {
     ...personData,
@@ -71,7 +95,6 @@ app.post('/api/persons', (req, res) => {
   };
 
   persons.push(newPerson);
-
   res.json(newPerson);
 });
 
